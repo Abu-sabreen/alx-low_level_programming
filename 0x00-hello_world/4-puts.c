45895985215523457e238755d2db2e5c1e22ba79
Abu-sabreen <adamuabdulfatah90@gmail.com>
2023-02-16 09:02:19 -0800

    task_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - this is the main function
- *
- * Return: always 0
+ * main - Entry
+ * Return: Always 0
  */
-init main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle);
-	return (0);
+		return (0);
 }
